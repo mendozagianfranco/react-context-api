629d6a6a0fa0837d582532ad48fc3e28f731ed60
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-04-23 14:56:40 +0200

    feat: decentralise data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
+import { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import axios from 'axios';
+import PostContext from './contexts/PostContext';
 import Homepage from './pages/Homepage';
 import About from './pages/About';
 import Posts from './pages/Posts';
@@ -6,18 +9,32 @@ import DefaultLayout from './layouts/DefaultLayout';
 import SinglePost from './components/SinglePost';
 
 function App() {
+  const [postsData, setPostsData] = useState([]);
+
+  const endpoint = 'https://jsonplaceholder.typicode.com/posts';
+
+  useEffect(() => {
+    axios.get(endpoint)
+      .then(res => {
+        setPostsData(res.data);
+      });
+  }, []);
+
+
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<DefaultLayout />}>
-            <Route index path='/' element={<Homepage />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/posts' element={<Posts />} />
-            <Route path='/posts/:id' element={<SinglePost />} />
-          </Route>
-        </Routes >
-      </BrowserRouter >
+      <PostContext.Provider value={postsData}>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<DefaultLayout />}>
+              <Route index path='/' element={<Homepage />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/posts' element={<Posts />} />
+              <Route path='/posts/:id' element={<SinglePost />} />
+            </Route>
+          </Routes >
+        </BrowserRouter >
+      </PostContext.Provider>
     </>
   );
 }
